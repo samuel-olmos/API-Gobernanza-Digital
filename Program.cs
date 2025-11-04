2a1c2d545c1bde20beca9d50fc8c0426bcf65e5b
ChordanDev <lucca.giordana@uap.edu.ar>
2025-11-03 22:45:59 -0300

    Cambio del program

--- Program.cs
+++ Program.cs
@@ -1,21 +1,33 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using API_Gobernanza_Digital.Data;
-using API_Gobernanza_Digital.Services;
 using API_Gobernanza_Digital.Interfaces;
+using API_Gobernanza_Digital.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
 
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+builder.Services.AddDbContext<GobernanzaDbContext>(options =>
+    options.UseSqlServer(connectionString)
+);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
 app.UseHttpsRedirection();
 
