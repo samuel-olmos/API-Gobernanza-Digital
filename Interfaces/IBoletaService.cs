0bdc3fbfe780eb0b323df112426265fef09610df
ChordanDev <lucca.giordana@uap.edu.ar>
2025-11-07 18:15:11 -0300

    Arreglo de BoletaService y BoletaDbService

--- Interfaces/IBoletaService.cs
+++ Interfaces/IBoletaService.cs
@@ -5,11 +5,18 @@ namespace API_Gobernanza_Digital.Interfaces;
 
 public interface IBoletaService
 {
+    // CRUD
     IEnumerable<Boleta> GetAll();
     Boleta? GetById(int id);
     Boleta Create(Boleta boleta);
     Boleta? Update(int id, Boleta boleta);
     bool Delete(int id);
 
+    // Métodos específicos
     Task<int> GenerarBoletasPeriodo(DateTime? fechaReferencia = null);
+    bool MarcarComoPagada(int id, DateTime? fechaPago = null);
+    int ActualizarBoletasVencidas();
+    IEnumerable<Boleta> GetByContribuyente(int contribuyenteId);
+    IEnumerable<Boleta> GetByEstado(EstadoBoleta estado);
+    Boleta? GetByCodigoPago(string codigo);
 }
